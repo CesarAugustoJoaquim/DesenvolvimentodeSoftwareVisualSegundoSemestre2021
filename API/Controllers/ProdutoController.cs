92e53523ccc4a8e09a377d5e216a9a1315b8035a
Cesar Augusto Joaquim <cesar.a.joaquim@gmail.com>
2021-08-31 16:33:38 -0300

    listar

--- API/Controllers/ProdutoController.cs
+++ API/Controllers/ProdutoController.cs
@@ -1,17 +1,36 @@
 using Microsoft.AspNetCore.Mvc;
 using API.Models;
+using API.Data;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace API.Controllers
 {
+
     [ApiController]
     [Route("api/produto")]
     public class ProdutoController : ControllerBase
     {
+        private readonly DataContext _context;
+        public ProdutoController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpPost]
         [Route("Create")]
-        public Produto Post(Produto produto)
+        public Produto Create(Produto produto)
         {
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
             return produto;
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public List<Produto> List()
+        {
+            return _context.Produtos.ToList();
         }
     }
 }
